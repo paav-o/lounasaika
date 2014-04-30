9b98d8198a6ffaec3bfb6b46772e83c32f80fc6f
Juhani Haapala <juhni@codaone.com>
2014-04-30 09:08:24 +0300

    Sodexo parser fix.

--- lib/_sodexo.rb
+++ lib/_sodexo.rb
@@ -1,12 +1,12 @@
 class Sodexo
   def self.get_meals(data, meals)
-    json_path = data.css(".print-link")[1]["href"].gsub("weekly_html", "weekly_json")
+    json_path = data.css("div#action_links_wrapper").css("a.json")[0]["href"].gsub("daily_json", "weekly_json")
     data = JSON.parse(open("http://www.sodexo.fi"+json_path).read)
 
     meals.keys.each do |locale|
       data["menus"].each_with_index do |day,index|
+        meals[locale][index] = []
         if day.last.first["title_#{locale}"].present?
-          meals[locale][index] = []
           day.last.each do |meal|
             meal_with_allergy_info = [meal["title_#{locale}"], " (", meal["properties"], ")"].join.gsub(" ()", "")
             meals[locale][index] << meal_with_allergy_info.strip
