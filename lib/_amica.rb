353d2bbf5b58a9c24cfddfe6c7bafbec9c7f73fc
Paavo Leinonen <paavo@protoverstas.com>
2014-09-01 15:31:52 +0200

    New scraper for Amica restaurant; Update some restauraunt URLs

--- lib/_amica.rb
+++ lib/_amica.rb
@@ -3,67 +3,25 @@
 class Amica
   def self.get_meals(data, meals)
     index = 0
-    days_regexp = /maanantai|tiistai|keskiviikko|torstai|perjantai|lauantai|ruokahalua|monday|tuesday|wednesday|thursday|friday|saturday|enjoy/mi
 
-    if data.at_css("#Ruokalista").present?
-      data = data.at_css("#Ruokalista")
-    else
-      data = data.at_css("#Menu")
-    end
-    
-	if data.nil?
-		raise "Menu not found"
-	end
-	
-   
-	day = data.css(".ContentArea")[1]
-	day.search("br").each { |br| br.replace("\n") }
-	meals.first.last[index] = []
+    data["LunchMenus"].each do |day|
+      menu = day["SetMenus"][0]
+      meals.first.last[index] = []
 
-	day.text.gsub(/(.*?)/mi, '').split(/\n/).each do |meal|
-		meal = meal.strip
-		   .gsub(/(\S\))/, '\1, ')
-		   .gsub(/, ,/, ", ")
-		   .gsub(/, +$/, "")
-		   .gsub(/,  $/, "")
-		   .gsub(/,  $/, "")
-		   .gsub(/ $/, "")
-		meal.slice!("Hyvää ruokahalua!")
-
-		if meal =~ days_regexp
-			index += 1
-			meals.first.last[index] = []
-		else
-			meals.first.last[index] << meal if meal.present?
-		end
-
-	end
-
-
-    # Those few restaurants using a table, like Meccala
-    if meals.first.last.length < 3
-      index = 0
-      data.css(".ContentArea")[1].search('//tr/td/p').each do |row|
-
-        row = row.text.strip
-                      .gsub(/(\S\))/, '\1, ')
-                      .gsub(/, +$/, "")
-
-        if row =~ days_regexp
-          index += 1
-          meals.first.last[index] = []
-        else
-          meals.first.last[index] = [] unless meals.first.last[index].present?
-          meals.first.last[index] << row if row.present?
+      if menu
+        menu["Meals"].each do |meal|
+          begin 
+            meals.first.last[index] = [] unless meals.first.last[index].any?
+            meals.first.last[index] << meal["Name"] if meal["Name"].present?
+          rescue Exception => e
+            puts e.message
+          end
         end
-        
       end
+      index += 1
     end
-  
-    # meals.first.last.reject!(&:empty?) if meals.first.last.present?
-	meals[meals.first.first] = meals.first.last[1..5]
-  
+
+    meals.first.last.reject!(&:empty?) if meals.first.last.present?
     return meals
   end
-  
 end
