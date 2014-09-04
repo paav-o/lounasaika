0b2c53fb73fd8ab2a132509800097dbd4304982d
Siim Haugas <haugas@gmail.com>
2014-09-04 15:38:59 +0300

    Sodexo crawler fix. restaurants.yml URI fix

--- lib/_sodexo.rb
+++ lib/_sodexo.rb
@@ -1,6 +1,7 @@
 class Sodexo
   def self.get_meals(data, meals)
-    json_path = data.css(".print-link")[1]["href"].gsub("weekly_html", "weekly_json")
+    json_path = data.css(".weekly")[0]["href"].gsub("weekly_html", "weekly_json")
+
     data = JSON.parse(open("http://www.sodexo.fi"+json_path).read)
 
     meals.keys.each do |locale|
